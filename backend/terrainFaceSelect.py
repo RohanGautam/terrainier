d5be8604c37e8a0ff94fb2330e31d6964a0b91c1
RohanGautam <rohan1gautam@gmail.com>
2020-01-11 13:10:45 +0530

    added python scripts

---
+++ backend/terrainFaceSelect.py
@@ -0,0 +1,27 @@
+import subprocess
+import shutil
+
+
+vectiler = '/home/rohan/Desktop/Cpp_files/vectiler/build/vectiler.out'
+tilex = '23447/23448'
+tiley = '15191/15192'
+tilez = '15' #zoom level
+misc_settings = '--terrain 1 --buildings 1 --terrainExtrusionScale 1.5 --buildingsExtrusionScale 1.9'.split()
+
+blender = "/home/rohan/Downloads/blender/blender"
+blenderScript = "/home/rohan/Desktop/Python_files/terrainFaceSelection/blender_script.py"
+# path info for the generated models which we will pas onto blender
+source = "/home/rohan/Desktop/Python_files/terrainFaceSelection/source/terrain.obj"
+exportPath = "/home/rohan/Desktop/Python_files/terrainFaceSelection/processed/terrain-2.obj"
+
+def moveGeneratedFile():
+    fileToMove = '.'.join([tilex.split('/')[0], tiley.split('/')[0], tilez])+'.obj'
+    print(f"Moving and renaming {fileToMove}")
+    shutil.move(fileToMove, f'source/terrain.obj')
+
+# 1. Generate and download tile specified by given tile coordinates
+subprocess.run([vectiler, '--tilex',tilex,'--tiley',tiley,'--tilez',tilez, *misc_settings])
+# 2. Move it to the `./source` folder
+moveGeneratedFile()
+# 3. Process it with blender
+subprocess.run([blender, '--background', '--python', blenderScript, '--', source, exportPath])
