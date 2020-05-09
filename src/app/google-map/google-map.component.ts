2dbee5ff276e1919a471677216a9163e83f67020
RohanGautam <rohan1gautam@gmail.com>
2020-05-09 12:46:52 +0800

    added options to the map

--- src/app/google-map/google-map.component.ts
+++ src/app/google-map/google-map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, ViewChildren, QueryList } from '@angular/core';
-import { } from 'googlemaps'; // the google maps types
+// import { } from 'googlemaps'; // the google maps types
 import { GoogleMapInitializeService } from '../google-map-initialize.service';
 
 @Component({
@@ -8,16 +8,25 @@ import { GoogleMapInitializeService } from '../google-map-initialize.service';
   styleUrls: ['./google-map.component.scss']
 })
 export class GoogleMapComponent implements OnInit {
-  mapHeight : number = 400;
-  mapWidth : number = 700;
+  mapHeight: number = 400;
+  mapWidth: number = 700;
   readonly ROOT_URL = 'http://127.0.0.1:5000'
   scriptAdded: boolean = false; // set to true after evaluating a promise from the service.
 
+  position: google.maps.LatLng;
+  options: google.maps.MapOptions;
+
   // injectable (obj initialization done for you, you just use the instance).
   // Remember to put public/pvt so that it's actually initialized.
-  constructor(public gMapServiceObj: GoogleMapInitializeService) {}
+  constructor(public gMapServiceObj: GoogleMapInitializeService) { }
   async ngOnInit() {
     this.scriptAdded = await this.gMapServiceObj.initializeGoogleMaps();
+    this.position = new google.maps.LatLng(1.334, 103.847);
+    this.options = {
+      center: this.position,
+      zoom: 10,
+      gestureHandling: 'greedy'
+    }
   }
 
 
